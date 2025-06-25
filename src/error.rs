5d7079e1cf17c4036387f5ded182c71fed3eb230
Aleksandr Provotorov <provotorov@inbox.ru>
2025-06-25 08:40:03 +0300

    error

---
+++ src/error.rs
@@ -0,0 +1,19 @@
+use std::fmt::Display;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum Error {
+    Custom(String),
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Custom(s) => write!(f, "Custom error: {s}"),
+            _ => write!(f, "{self:?}"),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+
+pub type Result<T> = core::result::Result<T, Error>;
